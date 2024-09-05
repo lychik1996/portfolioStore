cb46343f479cd3d78a5a7d50c5b104509f371699
Vitaliy Khatsey <vitaliy.khatsey@gmail.com>
2024-09-05 09:28:46 +0300

    add smoothe close open modaldrawer

--- app/(main)/_components/ModalDrawer/ModalDrawer.tsx
+++ app/(main)/_components/ModalDrawer/ModalDrawer.tsx
@@ -1,10 +1,11 @@
 'use client';
-import Portal from '@/components/Portal';
+
 import { useModalDrawer } from '@/store/use-modalDrawer';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
 import Item from './Item';
 import Link from 'next/link';
+import clsx from 'clsx';
 const arr = [
   {
     src: '1',
@@ -40,32 +41,73 @@ export default function ModalDrawer() {
   const [items, setItems] = useState(arr);
   const [subtotal, setSubtotal] = useState(0);
   const startX = useRef<number>(0);
-
-  const handleTouchStart = (e:TouchEvent) => {
+  const [translateX, setTranslateX] = useState<number>(0);
+  const [shouldClose, setShouldClose] = useState<boolean>(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  
+  const handleTouchStart = useCallback((e: TouchEvent) => {
     startX.current = e.touches[0].clientX;
-  };
+    setTranslateX(0);
+    setShouldClose(false);
+  }, []);
 
-  const handleTouchMove = (e:TouchEvent) => {
-    const currentX = e.touches[0].clientX; 
+  const handleTouchMove = useCallback((e: TouchEvent) => {
+    const currentX = e.touches[0].clientX;
     const distance = currentX - startX.current;
 
-    if (distance > 100) { 
-      onClose(); 
+    if (distance > 0) {
+      setTranslateX(distance);
     }
-  };
+    if (distance > 150) {
+      setShouldClose(true);
+    } else {
+      setShouldClose(false);
+    }
+  }, []);
+
+  const debounceClose = useCallback(()=>{
+    if(modalRef.current){
+      modalRef.current.style.transition = 'transform 0.5s ease-in-out';
+      modalRef.current.style.transform = 'translateX(100%)';
+      
+      }
+      setTimeout(onClose,500);
+  },[onClose]);
+
+  const handleTouchEnd = useCallback(() => {
+    if (shouldClose) {
+      
+      debounceClose();
+      
+    } else {
+      setTranslateX(0);
+    }
+  }, [shouldClose,debounceClose]);
+  
+  
+  useEffect(() => {
+    if (!isOpen) {
+      setTranslateX(0);
+    }
+  }, [isOpen]);
+  
 
   useEffect(() => {
     if (isOpen) {
       document.addEventListener('touchstart', handleTouchStart);
       document.addEventListener('touchmove', handleTouchMove);
+      document.addEventListener('touchend', handleTouchEnd);
 
       return () => {
         document.removeEventListener('touchstart', handleTouchStart);
         document.removeEventListener('touchmove', handleTouchMove);
+        document.removeEventListener('touchend', handleTouchEnd);
       };
     }
-  }, [isOpen]);
-  
+  }, [isOpen, handleTouchEnd, handleTouchMove, handleTouchStart]);
+
+
+  ///del
   useEffect(() => {
     const newSubtotal = arr.reduce((acum, i) => acum + i.price * i.count, 0);
     setSubtotal(newSubtotal);
@@ -83,67 +125,77 @@ export default function ModalDrawer() {
   };
   return (
     <>
-      {isOpen && (
-        <Portal referenceId="body" isOpen={isOpen}>
-          <div className="fixed z-50 h-[100vh] w-full flex flex-row bg-black/30">
-            <div onClick={onClose} className="flex-1"></div>
-            <div className=" bg-white flex flex-row p-6 md:p-10 w-full sm:w-[70%] md:w-[65%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
-              <div className="w-[95%] flex flex-col">
-                <div>
-                  <h3 className=" text-2xl xl:text-3xl mb-4">Shopping Cart</h3>
-                  <p className="text-base lg:text-lg text-slate-500">
-                    Buy <span className="font-bold text-black">$122.35</span>{' '}
-                    More And Get{' '}
-                    <span className="font-bold text-black">Free Shipping</span>
-                  </p>
-                </div>
-                <div className="flex-1 border-b-[1px] border-slate-700 overflow-y-scroll scrollbar-hide mt-3">
-                  <div className="flex flex-col gap-2">
-                    {items.map((item, i) => (
-                      <Item
-                        key={i}
-                        item={item}
-                        onCountChange={(newCount) =>
-                          updateItemCount(i, newCount)
-                        }
-                      />
-                    ))}
-                  </div>
-                </div>
-                <label className="cursor-pointer flex flex-row gap-2 items-center border-b-[1px] py-4 border-slate-700">
-                  <input type="checkbox" className="cursor-pointer size-5" />
-                  <p className="text-base lg:text-lg text-slate-500">
-                    For <span className="font-bold text-black">$10.00</span>{' '}
-                    please wrap the product
-                  </p>
-                </label>
-                <div className="flex flex-row items-center justify-between text-base lg:text-lg font-bold py-5">
-                  <h3>Subtotal</h3>
-                  <h3>
-                    ${' '}
-                    {subtotal % 1 === 0
-                      ? `${subtotal}.00`
-                      : `${subtotal.toFixed(2)}`}
-                  </h3>
-                </div>
-                <Link href={'/'} className="button text-center py-1">
-                  Checkut
-                </Link>
-                <Link
-                  href={'/'}
-                  className="text-center underline font-bold text-base md:text-xl pt-4"
-                >
-                  View Cart
-                </Link>
-              </div>
-              <IoMdClose
-                className="size-6 sm:size-7 cursor-pointer"
-                onClick={onClose}
-              />
+      <div
+        className={clsx(
+          'fixed z-50 h-[100vh] w-full flex flex-row bg-black/30',
+          isOpen ? 'block opacity-100' : 'opacity-0 pointer-events-none'
+        )}
+      >
+        <div onClick={debounceClose} className="flex-1"></div>
+        <div
+        ref={modalRef}
+          className={clsx(
+            'bg-white flex flex-row p-6 md:p-10 w-full sm:w-[70%] md:w-[65%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] transition-transform duration-500 ease-in-out fixed right-0 top-0 h-full',
+            
+          )}
+          style={{
+            transform: isOpen ? `translateX(${translateX}px)` : `translateX(100%)`,
+            transition: translateX > 0 ? 'none' : 'transform 0.5s ease-in-out',
+          }}
+         
+        >
+          <div className="w-[95%] flex flex-col">
+            <div>
+              <h3 className=" text-2xl xl:text-3xl mb-4">Shopping Cart</h3>
+              <p className="text-base lg:text-lg text-slate-500">
+                Buy <span className="font-bold text-black">$122.35</span>
+                More And Get
+                <span className="font-bold text-black">Free Shipping</span>
+              </p>
             </div>
+            <div className="flex-1 border-b-[1px] border-slate-700 overflow-y-scroll scrollbar-hide mt-3">
+              <div className="flex flex-col gap-2">
+                {items.map((item, i) => (
+                  <Item
+                    key={i}
+                    item={item}
+                    onCountChange={(newCount) => updateItemCount(i, newCount)}
+                  />
+                ))}
+              </div>
+            </div>
+            <label className="cursor-pointer flex flex-row gap-2 items-center border-b-[1px] py-4 border-slate-700">
+              <input type="checkbox" className="cursor-pointer size-5" />
+              <p className="text-base lg:text-lg text-slate-500">
+                For <span className="font-bold text-black">$10.00</span> please
+                wrap the product
+              </p>
+            </label>
+            <div className="flex flex-row items-center justify-between text-base lg:text-lg font-bold py-5">
+              <h3>Subtotal</h3>
+              <h3>
+                ${' '}
+                {subtotal % 1 === 0
+                  ? `${subtotal}.00`
+                  : `${subtotal.toFixed(2)}`}
+              </h3>
+            </div>
+            <Link href={'/'} className="button text-center py-1">
+              Checkut
+            </Link>
+            <Link
+              href={'/'}
+              className="text-center underline font-bold text-base md:text-xl pt-4"
+            >
+              View Cart
+            </Link>
           </div>
-        </Portal>
-      )}
+          <IoMdClose
+            className="size-6 sm:size-7 cursor-pointer"
+            onClick={debounceClose}
+          />
+        </div>
+      </div>
     </>
   );
 }
