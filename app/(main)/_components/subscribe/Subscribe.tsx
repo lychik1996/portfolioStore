4e8191ce535b7c8c215415628914c1e61bebb7cd
Vitaliy Khatsey <vitaliy.khatsey@gmail.com>
2024-09-24 14:15:10 +0300

    add transition

--- app/(main)/_components/subscribe/Subscribe.tsx
+++ app/(main)/_components/subscribe/Subscribe.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useTransition } from "react";
 import axios from 'axios';
 import { toast } from "sonner";
+import clsx from "clsx";
 
 
 export default function Subscribe() {
  const [email,setEmail] = useState('');
- const handeleSubscribe = async (e: React.FormEvent) => {
+ const [isLoading, startTransition] = useTransition();
+ const handeleSubscribe =  (e: React.FormEvent) => {
+
   e.preventDefault(); 
-  await axios.post('/api/subscribe', { email }) 
+  startTransition(async()=>{
+    await axios.post('/api/subscribe', { email }) 
     .then((res) => {
       toast.success(res.data.message);
       setEmail('');
@@ -19,6 +23,7 @@ export default function Subscribe() {
         const errorMessage = error.response?.data?.message || "Something went wrong";
         toast.error(errorMessage)
     })
+  })
 };
   return (
     <div className="flex flex-row items-center w-5/6 xl:w-7/12 pt-8 md:pt-16 justify-center">
@@ -33,9 +38,15 @@ export default function Subscribe() {
         <p className="opacity-50 text-center text-xs md:text-sm lg:text-base max-w-[600px]">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin 
         </p>
-        <form onSubmit={handeleSubscribe} className='flex flex-col items-center w-5/6 gap-4 sm:gap-7 mt-4'>
-            <input type="text"  value={email} onChange={(e)=>setEmail(e.target.value)} className=' outline-none text-xs md:text-base w-full sm:w-5/6 md:w-full  rounded-none py-1 pl-1 sm:py-3 sm:pl-3' placeholder='michael@ymail.com' style={{boxShadow:'0px 3.02px 3.15px 0px rgba(0, 0, 0, 0.01),0px 13.28px 6.52px 0px rgba(0, 0, 0, 0.02),0px 32.6px 13px 0px rgba(0, 0, 0, 0.02),0px 62.79px 25.48px 0px rgba(0, 0, 0, 0.02),0px 105.65px 46.85px 0px rgba(0, 0, 0, 0.03),0px 163px 80px 0px rgba(0, 0, 0, 0.04)'}}/>
-            <button className='button px-4'>Subscribe Now</button>
+        <form onSubmit={handeleSubscribe} className='flex flex-col items-center w-5/6 gap-4 sm:gap-7 mt-4' >
+            <input type="text"  value={email} onChange={(e)=>setEmail(e.target.value)} disabled={isLoading} className={clsx(
+              ' outline-none text-xs md:text-base w-full sm:w-5/6 md:w-full  rounded-none py-1 pl-1 sm:py-3 sm:pl-3',
+              isLoading?'opacity-50':'opacity-100'
+            )} placeholder='michael@ymail.com' style={{boxShadow:'0px 3.02px 3.15px 0px rgba(0, 0, 0, 0.01),0px 13.28px 6.52px 0px rgba(0, 0, 0, 0.02),0px 32.6px 13px 0px rgba(0, 0, 0, 0.02),0px 62.79px 25.48px 0px rgba(0, 0, 0, 0.02),0px 105.65px 46.85px 0px rgba(0, 0, 0, 0.03),0px 163px 80px 0px rgba(0, 0, 0, 0.04)'}}/>
+            <button className={clsx(
+              'button px-4',
+              isLoading?"opacity-50":'opacity-100'
+            )} disabled={isLoading}>{isLoading?'Subscribing...':'Subscribe Now'}</button>
         </form>
       </div>
       <div className='hidden md:block'>
