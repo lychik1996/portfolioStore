51567ec313cea6ea3a7228057b5ad42782e812a3
Vitaliy Khatsey <vitaliy.khatsey@gmail.com>
2024-09-24 13:59:28 +0300

    create subscribe back and add base postgresql

--- app/(main)/_components/subscribe/Subscribe.tsx
+++ app/(main)/_components/subscribe/Subscribe.tsx
@@ -1,7 +1,25 @@
-import shadows from '@mui/material/styles/shadows';
-import Image from 'next/image';
+'use client';
+
+import Image from "next/image";
+import React, { useState } from "react";
+import axios from 'axios';
+import { toast } from "sonner";
+
 
 export default function Subscribe() {
+ const [email,setEmail] = useState('');
+ const handeleSubscribe = async (e: React.FormEvent) => {
+  e.preventDefault(); 
+  await axios.post('/api/subscribe', { email }) 
+    .then((res) => {
+      toast.success(res.data.message);
+      setEmail('');
+    })
+    .catch((error) => {
+        const errorMessage = error.response?.data?.message || "Something went wrong";
+        toast.error(errorMessage)
+    })
+};
   return (
     <div className="flex flex-row items-center w-5/6 xl:w-7/12 pt-8 md:pt-16 justify-center">
         <div>
@@ -15,8 +33,8 @@ export default function Subscribe() {
         <p className="opacity-50 text-center text-xs md:text-sm lg:text-base max-w-[600px]">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin 
         </p>
-        <form action="" className='flex flex-col items-center w-5/6 gap-4 sm:gap-7 mt-4'>
-            <input type="text"  className=' outline-none text-xs md:text-base w-full sm:w-5/6 md:w-full  rounded-none py-1 pl-1 sm:py-3 sm:pl-3' placeholder='michael@ymail.com' style={{boxShadow:'0px 3.02px 3.15px 0px rgba(0, 0, 0, 0.01),0px 13.28px 6.52px 0px rgba(0, 0, 0, 0.02),0px 32.6px 13px 0px rgba(0, 0, 0, 0.02),0px 62.79px 25.48px 0px rgba(0, 0, 0, 0.02),0px 105.65px 46.85px 0px rgba(0, 0, 0, 0.03),0px 163px 80px 0px rgba(0, 0, 0, 0.04)'}}/>
+        <form onSubmit={handeleSubscribe} className='flex flex-col items-center w-5/6 gap-4 sm:gap-7 mt-4'>
+            <input type="text"  value={email} onChange={(e)=>setEmail(e.target.value)} className=' outline-none text-xs md:text-base w-full sm:w-5/6 md:w-full  rounded-none py-1 pl-1 sm:py-3 sm:pl-3' placeholder='michael@ymail.com' style={{boxShadow:'0px 3.02px 3.15px 0px rgba(0, 0, 0, 0.01),0px 13.28px 6.52px 0px rgba(0, 0, 0, 0.02),0px 32.6px 13px 0px rgba(0, 0, 0, 0.02),0px 62.79px 25.48px 0px rgba(0, 0, 0, 0.02),0px 105.65px 46.85px 0px rgba(0, 0, 0, 0.03),0px 163px 80px 0px rgba(0, 0, 0, 0.04)'}}/>
             <button className='button px-4'>Subscribe Now</button>
         </form>
       </div>
---
+++ app/api/subscribe/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+
+
+export const POST = async (req: Request) => {
+  const { email } = await req.json();
+  try {
+    const existingEmail = await db.subscribe.findUnique({
+      where: { email },
+    });
+    if (existingEmail) {
+      return new Response(JSON.stringify({ message: "Email already exists" }), { status: 400 });
+    }
+
+    const newSubscribe = await db.subscribe.create({
+      data: { email },
+    });
+
+    return new Response(JSON.stringify({ message: "Subscribed successfully!" }), { status: 201 });
+  } catch (error) {
+    return new Response(JSON.stringify({ message: "Something went wrong" }), { status: 500 });
+  }
+};
